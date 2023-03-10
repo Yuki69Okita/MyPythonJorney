f4359d63944fbc0193bc36236b374b8c320a8b67
Valentina Marinova <61772169+Yuki69Okita@users.noreply.github.com>
2023-03-10 20:19:06 +0200

    Add files via upload

---
+++ Lesson 8/Lesson 8.py
@@ -0,0 +1,75 @@
+# Lists - Used to store multiple items in a single variable
+# They are ordered, changeable, and allow duplicate values
+list1 = ["Hello", "My", "name", "is"]
+
+list1[1] = "again"
+print(list1)
+
+for item in list1:
+    print(item)
+
+# Indexing - 0 1 2 3..
+names = ["Gosho", "Ivan", "Maria", "Stanislav", "Todor"]
+print(names[2:4])
+
+#  ex 1 - Find the largest number
+numbers_list = [5, 10, 9, 1, 26, -3, 20]
+number = numbers_list[0]
+
+for x in numbers_list:
+    if x > number:
+        number = x
+print(number)
+
+# 2D List - Lists in list, just like matrix
+matrix = [
+    [1, 3, 4],
+    [3, 9, 2],
+    [8, 6, 7]
+]
+
+# Printing values one by one
+for row in matrix:
+    for col in row:
+        print(col)
+
+print(matrix[2][1:])  # Indexing is the same but with two [outer list][inner list]
+
+# Methods for lists:
+numbers_list = [5, 10, 9, 1, 26, -3, 20]
+print(numbers_list)
+
+numbers_list.append(2)  # Adds an element at the end of the list
+
+numbers_list.insert(0, 69)  # Adds an element at the specified position (index, new element)
+
+copy_list = numbers_list.copy()  # Returns a copy of the list
+print(copy_list)
+
+numbers_list.remove(26)  # Removes the first item with the specified value
+
+numbers_list.clear()  # Removes all the elements from the list
+
+numbers_list.pop()  # Removes the element at the specified position (if not specified - removes the last element)
+
+print(numbers_list.index(26))  # Returns the index of the first element with the specified value
+# (error when value is not found in the list)
+
+print(numbers_list.count(26))  # Returns the number of elements with the specified value
+
+numbers_list.sort()  # Sorts the list
+
+numbers_list.reverse()  # Reverses the order of the list
+
+names = ("Loki", "Toki")
+numbers_list.extend(names)  # Add the elements of a list (or any iterable), to the end of the current list
+
+# ex 2 - Remove duplicates from list
+numbers_list2 = [3, 4, 3, 4, 5, 6]
+duplicates = []
+
+for item in numbers_list2:
+    if item not in duplicates:
+        duplicates.append(item)
+
+print(duplicates)
---
+++ Lesson 8/README.md
@@ -0,0 +1,38 @@
+Lists:
+------
+- Used to store multiple items in a single variable
+- They are ordered, changeable, and allow duplicate values
+- Indexing is the same as strings
+------------------------
+2D Lists:
+-----------
+- Lists in list, just like matrix
+- Indexing is the same but with two [outer list][inner list]
+------------
+Methods for lists:
+--------------------
+- append() - Adds an element at the end of the list
+- insert() - Adds an element at the specified position (index, new element)
+- copy() - Returns a copy of the list
+- remove() - Removes the first item with the specified value
+- clear() - Removes all the elements from the list
+- pop() - Removes the element at the specified position (if not specified - removes the last element)
+- index() - Returns the index of the first element with the specified value
+  - (error when value is not found in the list)
+- count() - Returns the number of elements with the specified value
+- sort() - Sorts the list
+- reverse() - Reverses the order of the list
+- extend() - Add the elements of a list (or any iterable), to the end of the current list
+-------------
+
+Exercises:
+----------
+
+- Exercise 1 - Find the largest number
+	- You need to use list
+    - Find the largest number
+
+
+- Exercise 2 - Remove duplicates from list
+	- You need to use list
+    - Remove duplicates from list
